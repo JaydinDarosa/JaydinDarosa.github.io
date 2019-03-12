130039591b43adbc59ff28427605c059a6c64408
JaydinDarosa <jaydin.darosa01@gmail.com>
2019-03-11 21:22:55 -0400

    Fixed script main.js
    Now allowing code to run at document ready

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 var quotes = [];
 quotes[0] = "Any sufficiently advanced technology is indistinguishable from magic -Arthur C. Clarke"
 quotes[1] = "Privacy - like eating and breathing - is one of life's basic requirements -Katherine Neville"
-document.getElementById("quote").innerHTML = quotes[Math.floor(Math.random()*quotes.length)];
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("quote").innerHTML = quotes[Math.floor(Math.random()*quotes.length)];
+
+}, false);
